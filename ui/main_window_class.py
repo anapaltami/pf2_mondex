207498f525ffc3481c9933cb26fee7164398dc5b
Anastasia Altamirano <anapaltami@gmail.com>
2024-07-24 13:35:18 -1000

    show selected monster details on right panel of search tab

--- ui/main_window_class.py
+++ ui/main_window_class.py
@@ -2,7 +2,7 @@ import sqlite3
 import pandas as pd
 from PyQt5.QtWidgets import (
     QVBoxLayout, QLineEdit, QWidget, QPushButton, QTableWidgetItem, QTableWidget, QFormLayout, QSpinBox, QComboBox,
-    QLabel, QTabWidget, QTextEdit
+    QLabel, QTabWidget, QTextEdit, QHBoxLayout, QHeaderView
 )
 from src.config import DB_FILE
 from models.pf2_newmon_model import NewMonsterModel
@@ -40,23 +40,38 @@ class MainWindow(QWidget):
 
     def search_tab(self):
         search_tab = QWidget()
-        layout = QVBoxLayout()
+        layout = QHBoxLayout()
+
+        # left panel for search and table
+        left_panel = QVBoxLayout()
 
         # search bar
         self.search_bar = QLineEdit(self)
-        self.search_bar.setPlaceholderText('Search')
-        self.search_bar.textChanged.connect(self.search) # dynamic searching
-        layout.addWidget(self.search_bar)
+        self.search_bar.setPlaceholderText('Search by name or trait')
+        self.search_bar.textChanged.connect(self.search)  # dynamic searching
+        left_panel.addWidget(self.search_bar)
 
         # search button
         self.search_button = QPushButton('Search', self)
         self.search_button.clicked.connect(self.search)
-        layout.addWidget(self.search_button)
+        left_panel.addWidget(self.search_button)
 
         # data display table
         self.table = QTableWidget(self)
-        layout.addWidget(self.table)
+        self.table.setSelectionBehavior(QTableWidget.SelectRows)
+        self.table.setSelectionMode(QTableWidget.SingleSelection)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table.itemSelectionChanged.connect(self.display_monster_details)
+        left_panel.addWidget(self.table)
 
+        layout.addLayout(left_panel)
+
+        # right panel for monster details
+        self.details_panel = QVBoxLayout()
+        self.details_label = QLabel("Select a monster to see its details.")
+        self.details_panel.addWidget(self.details_label)
+
+        layout.addLayout(self.details_panel)
         search_tab.setLayout(layout)
         return search_tab
 
@@ -126,6 +141,28 @@ class MainWindow(QWidget):
     def search(self):
         query = self.search_bar.text()
         self.load_data(query)
+
+    def display_monster_details(self):
+        selected_items = self.table.selectedItems()
+        if not selected_items:
+            return
+
+        selected_name = selected_items[0].text()
+        conn = sqlite3.connect(DB_FILE)
+        query = f"SELECT * FROM pf2_monsters WHERE Name = ?"
+        df = pd.read_sql_query(query, conn, params=(selected_name,))
+        conn.close()
+
+        if df.empty:
+            return
+
+        monster = df.iloc[0]
+        details = f"Name: {monster['Name']}\nTraits: {monster['Traits']}\n"
+        details += f"Level: {monster['Level']}\nStr: {monster['Str']}\nDex: {monster['Dex']}\n"
+        details += f"Con: {monster['Con']}\nInt: {monster['Int']}\nWis: {monster['Wis']}\nCha: {monster['Cha']}\n"
+        # TODO Add more details to display
+
+        self.details_label.setText(details)
 
     def get_monster_traits(self):
         conn = sqlite3.connect(DB_FILE)
