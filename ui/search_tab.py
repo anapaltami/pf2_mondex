865b635834a36554b6a0f45bef572fcb6c7c1aa5
Anastasia Altamirano <anapaltami@gmail.com>
2024-07-28 11:33:19 -1000

    add ability to search through both pf2 monsters and user generated monsters on the search tab

--- ui/search_tab.py
+++ ui/search_tab.py
@@ -2,7 +2,7 @@ import sqlite3
 import pandas as pd
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLineEdit, QPushButton, QTableWidget, QHeaderView, \
     QLabel, QTableWidgetItem
-from src.config import DB_FILE
+from src.config import DB_FILE, GENERATED_DB_FILE
 
 
 class SearchTab(QWidget):
@@ -20,7 +20,7 @@ class SearchTab(QWidget):
         # search bar
         self.search_bar = QLineEdit(self)
         self.search_bar.setPlaceholderText('Search by name or trait')
-        self.search_bar.textChanged.connect(self.search)  # dynamic searching
+        self.search_bar.textChanged.connect(self.search)
         left_panel.addWidget(self.search_bar)
 
         # search button
@@ -48,21 +48,34 @@ class SearchTab(QWidget):
         self.load_data()
 
     def load_data(self, query=""):
+        # load and query PF2 monster database
         conn = sqlite3.connect(DB_FILE)
         if query:
             sql_query = (f"SELECT Name, Traits FROM pf2_monsters "
                          f"WHERE Name LIKE '%{query}%' OR Traits LIKE '%{query}%'")
         else:
             sql_query = f"SELECT Name, Level, Traits FROM pf2_monsters"
-        df = pd.read_sql_query(sql_query, conn)
+        df_main = pd.read_sql_query(sql_query, conn)
         conn.close()
 
-        self.table.setRowCount(len(df))
-        self.table.setColumnCount(len(df.columns))
-        self.table.setHorizontalHeaderLabels(df.columns)
+        # load and query user generated monster database
+        conn = sqlite3.connect(GENERATED_DB_FILE)
+        if query:
+            sql_query = (f"SELECT Name, Traits FROM generated_monsters "
+                         f"WHERE Name LIKE '%{query}%' OR Traits LIKE '%{query}%'")
+        else:
+            sql_query = f"SELECT Name, Level, Traits FROM generated_monsters"
+        df_generated = pd.read_sql_query(sql_query, conn)
+        conn.close()
 
-        for row_index, row in df.iterrows():
-            for col_index, col in enumerate(df.columns):
+        df_combined = pd.concat([df_main, df_generated], ignore_index=True)
+
+        self.table.setRowCount(len(df_combined))
+        self.table.setColumnCount(len(df_combined.columns))
+        self.table.setHorizontalHeaderLabels(df_combined.columns)
+
+        for row_index, row in df_combined.iterrows():
+            for col_index, col in enumerate(df_combined.columns):
                 self.table.setItem(row_index, col_index, QTableWidgetItem(str(row[col])))
 
     def search(self):
@@ -77,16 +90,23 @@ class SearchTab(QWidget):
         selected_name = selected_items[0].text()
         conn = sqlite3.connect(DB_FILE)
         query = f"SELECT * FROM pf2_monsters WHERE Name = ?"
-        df = pd.read_sql_query(query, conn, params=(selected_name,))
+        df_main = pd.read_sql_query(query, conn, params=(selected_name,))
         conn.close()
 
-        if df.empty:
+        conn = sqlite3.connect(GENERATED_DB_FILE)
+        query = f"SELECT * FROM generated_monsters WHERE Name = ?"
+        df_generated = pd.read_sql_query(query, conn, params=(selected_name,))
+        conn.close()
+
+        df_combined = pd.concat([df_main, df_generated], ignore_index=True)
+
+        if df_combined.empty:
             return
 
-        monster = df.iloc[0]
-        details = f"Name: {monster['Name']}\nTraits: {monster['Traits']}\n"
-        details += f"Level: {monster['Level']}\nStr: {monster['Str']}\nDex: {monster['Dex']}\n"
-        details += f"Con: {monster['Con']}\nInt: {monster['Int']}\nWis: {monster['Wis']}\nCha: {monster['Cha']}\n"
+        monster = df_combined.iloc[0]
+        details = (f"Name: {monster['Name']}\nTraits: {monster['Traits']}\nLevel: {monster['Level']}\n"
+                   f"Str: {monster['Str']}\nDex: {monster['Dex']}\nCon: {monster['Con']}\nInt: {monster['Int']}\n"
+                   f"Wis: {monster['Wis']}\nCha: {monster['Cha']}\n")
         # TODO Add more details to display
 
         self.details_label.setText(details)
